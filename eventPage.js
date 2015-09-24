e3fef033fddcb1c2b5099a29c286417cdc21a602
Yining <ys2189@nyu.edu>
2015-09-24 09:12:03 -0400

    first commit

---
+++ eventPage.js
@@ -0,0 +1,3 @@
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  alert("Goodbye~ Plz do some work or go to sleep:)");
+});
---
+++ popup.js
@@ -0,0 +1,95 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+var netflix = false; 
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // console.log(url);
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    //if(url.indexOf("netflix") > -1){
+      callback(url);
+      if(url.indexOf("netflix") > -1){
+        netflix = true;
+      }
+      else{
+        netflix = false;
+    }
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    
+    // Put the image URL in Google search.
+    renderStatus('Are you on Netflix? ' + url);
+    //renderStatus('Are you on Netflix?' + url);
+  });
+});
+
+var alarmClock = {
+
+        onHandler : function(e) {
+          if(netflix){
+            chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.2} );}
+                    window.close();
+        },
+
+        offHandler : function(e) {
+            chrome.alarms.clear("myAlarm");
+                    window.close();
+        },
+
+        setup: function() {
+            var a = document.getElementById('alarmOn');
+            a.addEventListener('click',  alarmClock.onHandler );
+            var a = document.getElementById('alarmOff');
+            a.addEventListener('click',  alarmClock.offHandler );
+        }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    alarmClock.setup();
+});
